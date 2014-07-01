312f0ec3fa6a553368a5d279d81e54d090672ece
Lauren M <mlmxwell@gmail.com>
2014-07-01 12:49:48 -0500

    Initial Commit

---
+++ app/mu-plugins/custom-content.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+Plugin Name: Custom Post Types and Custom Post Meta
+Description: Adds all the post type filters.
+Author: Lauren Maxwell
+Version: 1.0
+*/
+
+add_action( 'init', 'create_post_type' );
+
+function create_post_type() {
+
+
+  /*****************************************************?
+   * Galleries
+   */
+  register_post_type( 'slideshow_content',
+    array(
+      'labels' => array(
+        'name' => __( 'Slideshow' ),
+        'singular_name' => __( 'Slideshow' )
+      ),
+      'public' => true,
+      'has_archive' => true,
+      'show_in_nav_menus' => false,
+      'menu_position' => 0,
+      'supports' => array('title'),
+      'rewrite' => array( 'slug' => 'slides' ),
+      'taxonomies' => array('category'),
+      'hierarchical' => true
+    )
+  );
+
+}
