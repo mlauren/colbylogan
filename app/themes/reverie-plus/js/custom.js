29078ab29e85c9944c2063f371b9cd9cfd78c202
luran <mlmaxwell@gmail.com>
2014-07-08 12:32:04 -0600

    adds fix to js

--- app/themes/reverie-plus/js/custom.js
+++ app/themes/reverie-plus/js/custom.js
@@ -33,30 +33,35 @@ jQuery(document).ready(function() {
   });
 
   jQuery('.single-item-videos').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 2,
-
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: true,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: true,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
+	infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
       }
-    ]
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
   });
 
 });
